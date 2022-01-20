6aa09df01acf80d13f48bfc144d36ea074380b61
temportalflux <dustin.yost.t@gmail.com>
2022-01-20 18:12:41 -0500

    Bump crates

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,8 +1,6 @@
 use crystal_sphinx::plugin;
 use crystal_sphinx_editor::run;
-use engine::utility::VoidResult;
 
-fn main() -> VoidResult {
-	run(plugin::Config::default().with(vanilla::Plugin::default()))?;
-	Ok(())
+fn main() {
+	engine::run(|| run(plugin::Config::default().with(vanilla::Plugin::default())));
 }
--- game/src/main.rs
+++ game/src/main.rs
@@ -1,7 +1,5 @@
 use crystal_sphinx::{plugin, run};
-use engine::utility::VoidResult;
 
-fn main() -> VoidResult {
-	run(plugin::Config::default().with(vanilla::Plugin::default()))?;
-	Ok(())
+fn main() {
+	engine::run(|| run(plugin::Config::default().with(vanilla::Plugin::default())));
 }
